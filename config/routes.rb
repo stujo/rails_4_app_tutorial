b4e218786a362799718b918152e3933aacf57bf7
stujo <stujo@imagescript.com>
2014-05-27 17:11:05 -0700

    Added route to PagesController helloworld

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'helloworld' => 'pages#helloworld', as: :helloworld
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
